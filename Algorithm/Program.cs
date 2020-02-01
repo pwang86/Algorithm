f3bb6ee7160f3daded35f9769506ff870d3b601e
pwang86 <pwang86@gmail.com>
2020-02-01 23:06:46 +1100

    add reverse integer

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -16,12 +16,13 @@ namespace Algorithm
 			//string input = Console.ReadLine();
 			//string[] intArray = input.Split(',');
 			int[] a = new int[]{-3, 4, 3, 90 };
-			int[] b = TwoSum(a, 0);
-			Console.WriteLine("Result is :");
-			foreach (var item in b)
-			{
-				Console.WriteLine("{0}",item);
-			}
+			//int[] b = TwoSum(a, 0);
+			//Console.WriteLine("Result is :");
+			//foreach (var item in b)
+			//{
+			//	Console.WriteLine("{0}",item);
+			//}
+			Reverse(123);
 		}
 
 		public static int[] TwoSum(int[] nums, int target)
@@ -62,6 +63,23 @@ namespace Algorithm
 			//}
 			throw new Exception("No two sum solution");
 		}
-	
-}
+		public static int Reverse(int x)
+		{
+			int reverse = 0;
+			while (x != 0)
+			{
+				int remainder = x % 10;
+				x /= 10;
+				Console.WriteLine(x);
+				if (reverse > Int32.MaxValue / 10 || (reverse == Int32.MaxValue / 10 && remainder > 7))
+				{ return 0; }
+				if (reverse < Int32.MinValue / 10 || (reverse == Int32.MinValue / 10 && remainder < -8))
+				{ return 0; }
+				reverse = (reverse * 10) + remainder;
+
+			}
+			return reverse;
+		}
+
+	}
 }
