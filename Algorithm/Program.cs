a87c4233042fe4e6c655702731d7f50f5d74e92c
pwang86 <pwang86@gmail.com>
2020-02-17 21:58:18 +1100

    add same tree

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -498,5 +498,34 @@ namespace Algorithm
 		//	}
 		//	return head;
 		//}
-	}
+
+		//public static bool IsSameTree(TreeNode p, TreeNode q)
+		//{
+		//	if (p == null && q == null)
+		//		return true;
+		//	else if (p == null || q == null)
+		//		return false;
+		//	bool result = false;
+		//	if (p.val != q.val)
+		//	{
+		//		return false;
+		//	}
+		//	else
+		//	{
+		//		if (IsSameTree(p.left, q.left))
+		//		{
+		//			if (IsSameTree(p.right, q.right))
+		//			{
+		//				result = true;
+		//			}
+		//			else
+		//				return false;
+		//		}
+		//		else
+		//			return false;
+		//	}
+		//	return result;
+		//}
+	
+}
 }
