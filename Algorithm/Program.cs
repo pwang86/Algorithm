4d78856b1740a679cfc57053522e4e360bd57ea5
pwang86 <pwang86@gmail.com>
2020-02-18 20:50:50 +1100

    add symmetric tree

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -526,6 +526,37 @@ namespace Algorithm
 		//	}
 		//	return result;
 		//}
-	
-}
+
+		/**
+          * Definition for a binary tree node.
+		  * public class TreeNode {
+          *     public int val;
+		  *     public TreeNode left;
+		  *     public TreeNode right;
+		  *     public TreeNode(int x) { val = x; }
+          * }
+        */
+		// recursion
+		//public class Solution
+		//{
+		//	public bool IsSymmetric(TreeNode root)
+		//	{
+		//		return root == null || this.IsSymmetricRecursion(root.left, root.right);
+		//	}
+
+		//	public bool IsSymmetricRecursion(TreeNode node1, TreeNode node2)
+		//	{
+		//		if (node1 == null && node2 == null)
+		//		{
+		//			return true;
+		//		}
+		//		else if (node1 == null || node2 == null)
+		//			return false;
+		//		if (node1.val != node2.val)
+		//			return false;
+		//		return this.IsSymmetricRecursion(node1.left, node2.right) && this.IsSymmetricRecursion(node1.right, node2.left);
+		//	}
+		//}
+
+	}
 }
