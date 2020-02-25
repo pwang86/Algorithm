cbce95b51989e2b6a1341bacf4021ec3ba234aab
pwang86 <pwang86@gmail.com>
2020-02-25 23:17:11 +1100

    add path sum

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -733,5 +733,14 @@ namespace Algorithm
 		//	}
 		//	return height;
 		//}
+
+		//public static bool HasPathSum(TreeNode root, int sum)
+		//{
+		//	if (root == null) return false;
+		//	if (root.left == null && root.right == null)
+		//		return (sum == root.val);
+		//	return HasPathSum(root.left, sum - root.val) || HasPathSum(root.right, sum - root.val);
+
+		//}
 	}
 }
