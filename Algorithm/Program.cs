04d606696edd0bf66f79d11469253dcdeaf31e16
pwang86 <pwang86@gmail.com>
2020-03-03 22:28:23 +1100

    add two sum II

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -938,5 +938,29 @@ namespace Algorithm
 		//	}
 		//	return _mStack.Peek().Item2;
 		//}
+
+		//public static int[] TwoSum(int[] numbers, int target)
+		//{
+		//	for (int i = 0, j = numbers.Length - 1; i < j;)
+		//	{
+		//		if (numbers[i] + numbers[j] == target)
+		//		{
+		//			return new int[] { i + 1, j + 1 };
+		//		}
+		//		else
+		//		{
+		//			if (numbers[i] + numbers[j] < target)
+		//			{
+		//				i++;
+		//			}
+		//			else
+		//			{
+		//				j--;
+		//			}
+		//		}
+
+		//	}
+		//	return new int[] { -1, -1 };
+		//}
 	}
 }
