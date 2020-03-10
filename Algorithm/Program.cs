849a9acda86cb354538794b3206734e9b5a36460
pwang86 <pwang86@gmail.com>
2020-03-10 21:54:31 +1100

    number of 1 bit

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1083,5 +1083,21 @@ namespace Algorithm
 			return res;
 		}
 
+		public static int HammingWeight(uint n)
+		{
+			int bits = 0;
+			int mask = 1;
+			for (int i = 0; i < 32; i++)
+			{
+				if ((n & mask) != 0)
+				{
+					bits++;
+				}
+				mask <<= 1;
+			}
+			return bits;
+		}
+
+
 	}
 }
