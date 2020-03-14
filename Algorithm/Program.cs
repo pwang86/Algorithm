2ac282e195ee80886a789a3a8f76049b668316a4
pwang86 <pwang86@gmail.com>
2020-03-14 23:20:04 +1100

    add isomorphic

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1149,20 +1149,26 @@ namespace Algorithm
 
 		public static int CountPrimes(int n)
 		{
-			if (n == 0 || n == 1)
-				return 0;
-			if (n == 2)
-				return 1;
-			int count = 0;
-			for (int i = 3; i <= n; i++)
+			var isNotPrime = new bool[n];
+
+			for (var i = 2; i < n; i++)
 			{
-				i / ((int)Math.Sqrt(i));
-	
-			if (i)
+				if (isNotPrime[i]) continue;
+				var start = i + i;
+				while (start < n)
+				{
+					isNotPrime[start] = true;
+					start += i;
+				}
+			}
+			// remove 0 and 1
+			return isNotPrime.Skip(2).Count(x => x == false);
+		}
+
+		public static bool IsIsomorphic(string s, string t)
+		{
 
 		}
-			return count;
 
-		}
 	}
 	}
