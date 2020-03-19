fc3f67e95f6c5a784287b510a725ab2fcd2fd8d1
pwang86 <pwang86@gmail.com>
2020-03-19 22:32:22 +1100

    init implementing stack using queues

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -1310,5 +1310,39 @@ namespace Algorithm
 		//	return false;
 
 		//}
+
+		//public class MyStack
+		//{
+
+		//	/** Initialize your data structure here. */
+		//	public MyStack()
+		//	{
+
+		//	}
+
+		//	/** Push element x onto stack. */
+		//	public void Push(int x)
+		//	{
+
+		//	}
+
+		//	/** Removes the element on top of the stack and returns that element. */
+		//	public int Pop()
+		//	{
+
+		//	}
+
+		//	/** Get the top element. */
+		//	public int Top()
+		//	{
+
+		//	}
+
+		//	/** Returns whether the stack is empty. */
+		//	public bool Empty()
+		//	{
+
+		//	}
+		//}
 	}
 	}
